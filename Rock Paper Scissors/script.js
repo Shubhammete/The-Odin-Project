eb69abd232c85399c4c16ed0225a0a0ba5183389
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2023-12-15 17:10:27 +0530

    Changed UI of Rock-Paper-Scissor Game

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,49 +1,61 @@
 let icons = document.getElementsByClassName('icon')
 let status = document.querySelector('.status')
-
+let result
+let value
+let comp
 for(let i = 0;i<icons.length;i++){
 icons[i].addEventListener('click',(e)=>{
-    let value = e.target.alt
-    let comp = getComputerResponse()
-    if(value === comp){
-        status.innerHTML+=`<h1>Match Tied</h1>`
+    
+     value = e.target.alt
+     comp = getComputerResponse(value)
+    if(value === comp.name){
+        result+=`Match Tied`
     }else {
-        switch(comp){
+        switch(comp.name){
             case 'Stone':
                 if(value === 'Paper'){
-                    status.innerHTML+=`<h1>You Win</h1>`
+                    result=`You Win`
                 }else{
-                    status.innerHTML+=`<h1>Match Lose</h1>`
+                    result=`Match Lose`
                 }
                 break;
             case 'Paper':
                 if(value === 'Scissor'){
-                    status.innerHTML+=`<h1>You Win</h1>`
+                    result=`You Win`
                 }else{
-                    status.innerHTML+=`<h1>Match Lose</h1>`
+                    result=`Match Lose`
                 }
                 break;
             case 'Scissor':
                 if(value === 'Stone'){
-                    status.innerHTML+=`<h1>You Win</h1>`
+                    result=`You Win`
                 }else{
-                    status.innerHTML+=`<h1>Match Lose</h1>`
+                    result=`Match Lose`
                 }
                 break;
         }
     }
+    status.innerHTML = `<div>
+    <h3>Computer's Choice</h3>
+    <img src=${comp.img} height="150px" alt="scissor">
+    <h3>${comp.name}</h3>
+    </div>
+    <h3>${result}</h3>
+    <div>
+    <h3>Your's Choice</h3>
+    <img src=${e.target.src} height="150px" alt="scissor">
+    <h3>${value}</h3>
+    </div>`
+    result=""
 })
 }
 
-function getComputerResponse(){
+function getComputerResponse(value){
     
     let imgvalues = ["./images/granite.png","./images/paper-plane.png","./images/scissor.png"]
     let name = ["Stone","Paper","Scissor"]
     let val = Math.floor(Math.random()*imgvalues.length)
-    status.innerHTML = `<div>
-    <img src=${imgvalues[val]} height="150px" alt="scissor">
-    <p>${name[val]}</p>
-    </div>`
     
-    return name[val]
+    
+    return {name: name[val], img:imgvalues[val]}
 }
